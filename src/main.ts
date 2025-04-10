d23ee0198bf2d00c12ee8ac7938a22b52f6baab4
maksiho5 <podolakmaksim75@gmail.com>
2025-04-10 16:08:42 +0300

    set port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { join } from "path";
 import * as express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // загружаем переменные окружения
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
-  await app.listen(process.env.PORT ?? 5000);
-}
 
+  app.enableCors();
+
+  // Раздача статики (например, для изображений)
+  app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server running on port ${port}`);
+}
 bootstrap();
